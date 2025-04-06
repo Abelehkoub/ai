dd0ef9a3ee34ef7c027170b23f131ba5a1b66976
Abelehkoub <167072041+Abelehkoub@users.noreply.github.com>
2025-04-06 20:41:54 +0330

    create

--- Kagel/Phyton/pythonProject2/P2.py
+++ Kagel/Phyton/pythonProject2/P2.py
@@ -1,23 +1,20 @@
-import pandas as pd
+flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
+
+flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold"]
+
+print("++++++++++++++++++++++++++++++++++++++")
+print(flowers_list)
+print(flowers_list[2:])
+print("------------------------------")
+print(flowers_list)
+print(flowers_list[-2:])
+print("------------------------------")
+print(flowers_list)
+print(flowers_list[:2])
+print("------------------------------")
+print(flowers_list)
+print(flowers_list[:-2])
+print("------------------------------")
 
 
-
-# save filepath to variable for easier access
-
-
-melbourne_file_path = 'd:/Git/AI/Kagel/winemag-data-130k-v2.csv'
-#melbourne_file_path = 'melb_data.csv'0000
-
-# read the data and store data in DataFrame titled melbourne_data
-#melbourne_data = pd.read_csv(melbourne_file_path, index_col="price")
-reviews = pd.read_csv(melbourne_file_path)
-
-print(reviews.head())
-review_points_mean = reviews.points.mean()
-print(reviews.points.map(lambda p: p - review_points_mean))
-
-import numpy as np
-import pandas as pd
-df = pd.DataFrame(data=np.random.randint(0,10,(5,7)))
-print(df)
-df.apply(sum)
+#
